b90ac11ce8087b6c2b4190a700c37f9cc6079001
MD Tarek Islam <45341688+n30-Joker@users.noreply.github.com>
2024-11-04 20:35:23 +0000

    Completed comments for car
    v1.3 added completed comments for the car module

--- cars.py
+++ cars.py
@@ -21,7 +21,7 @@ class Car:
         self.weekOrMore = weekOrMore
         self.VIP = VIP
 
-    def getBelowWeekRate(self):
+    def getBelowWeekRate(self): 
         return self.belowWeek
     
     def getWeekOrMoreRate(self):
@@ -31,13 +31,49 @@ class Car:
         return self.VIP
     
 class Hatchback(Car):
+    """
+    Hatchback car class with predefined daily rates.
+
+    Inherits all the methods from the Car class and sets 
+    specific daily rates.
+    """
     def __init__(self):
+        """
+        Initialises a Hatchback instance with the following rates:
+            - less than 7 days at £30 per day;
+            - 7 days or more at £25 per day;
+            - a special rate of £20 per day for VIP customers.
+        """
         super().__init__(30, 25, 20)
 
 class Sedan(Car):
+    """
+    Sedan car class with predefined rates.
+
+    Inherits methods from Car class and specifies
+    the daily rates.
+    """
     def __init__(self):
+        """
+        Initialises a Sedan instance with the following rates:
+            - less than 7 days at £50 per day;
+            - 7 days or more at £40 per day;
+            - a special rate of £35 per day for VIP customers.
+        """
         super().__init__(50, 40, 35)
 
 class SUV(Car):
+    """
+    SUV car class with determined daily rates.
+
+    Inherits methods from Car class and states
+    the daily rates.
+    """
     def __init__(self):
+        """
+        Initialises a SUV instance with the following rates:
+            - less than 7 days at £100 per day;
+            - 7 days or more at £90 per day;
+            - a special rate of £80 per day for VIP customers.
+        """
         super().__init__(100, 90, 80)
